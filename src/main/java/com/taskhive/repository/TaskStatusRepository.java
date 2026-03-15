f00eb202c7bcc4c0a8a8a5092a7f1caaf52d357d
c0nf1dxnt <c0nf1dxnt@yandex.ru>
2026-03-15 19:49:05 +0300

    feat: add JPA repositories for core entities

---
+++ src/main/java/com/taskhive/repository/TaskStatusRepository.java
@@ -0,0 +1,7 @@
+package com.taskhive.repository;
+
+import com.taskhive.model.TaskStatus;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TaskStatusRepository extends JpaRepository<TaskStatus, Long> {
+}
---
+++ src/main/java/com/taskhive/repository/WorkspaceRepository.java
@@ -0,0 +1,9 @@
+package com.taskhive.repository;
+
+import com.taskhive.model.Workspace;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.UUID;
+
+public interface WorkspaceRepository extends JpaRepository<Workspace, UUID> {
+}
